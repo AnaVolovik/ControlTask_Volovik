73f832d6bd32428cf9fe70bf1fa4b64b5c281301
AnaVolovik <nastasia.volovik@gmail.com>
2024-11-27 18:27:30 +0300

    chore: remove unnecessary whitespace and unused functions and comments

--- webapp/controller/NotFound.controller.js
+++ webapp/controller/NotFound.controller.js
@@ -1,19 +1,15 @@
 sap.ui.define([
-		"ControlTaskVolovik/ControlTaskVolovik/controller/BaseController"
-	], function (BaseController) {
-		"use strict";
+    "ControlTaskVolovik/ControlTaskVolovik/controller/BaseController"
+  ], function (BaseController) {
+    "use strict";
 
-		return BaseController.extend("ControlTaskVolovik.ControlTaskVolovik.controller.NotFound", {
+    return BaseController.extend("ControlTaskVolovik.ControlTaskVolovik.controller.NotFound", {
 
-			/**
-			 * Navigates to the worklist when the link is pressed
-			 * @public
-			 */
-			onLinkPressed : function () {
-				this.getRouter().navTo("worklist");
-			}
+      onLinkPressed : function () {
+        this.getRouter().navTo("worklist");
+      }
 
-		});
+    });
 
-	}
+  }
 );
--- webapp/controller/Object.controller.js
+++ webapp/controller/Object.controller.js
@@ -1,147 +1,90 @@
-/*global location*/
 sap.ui.define([
-		"ControlTaskVolovik/ControlTaskVolovik/controller/BaseController",
-		"sap/ui/model/json/JSONModel",
-		"sap/ui/core/routing/History",
-		"ControlTaskVolovik/ControlTaskVolovik/model/formatter"
-	], function (
-		BaseController,
-		JSONModel,
-		History,
-		formatter
-	) {
-		"use strict";
+    "ControlTaskVolovik/ControlTaskVolovik/controller/BaseController",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/routing/History",
+    "ControlTaskVolovik/ControlTaskVolovik/model/formatter"
+  ], function (
+    BaseController,
+    JSONModel,
+    History,
+    formatter
+  ) {
+    "use strict";
 
-		return BaseController.extend("ControlTaskVolovik.ControlTaskVolovik.controller.Object", {
+    return BaseController.extend("ControlTaskVolovik.ControlTaskVolovik.controller.Object", {
 
-			formatter: formatter,
+      formatter: formatter,
 
-			/* =========================================================== */
-			/* lifecycle methods                                           */
-			/* =========================================================== */
+      onInit : function () {
+        const oViewModel = new JSONModel({
+          busy : true,
+          delay : 0
+        });
 
-			/**
-			 * Called when the worklist controller is instantiated.
-			 * @public
-			 */
-			onInit : function () {
-				// Model used to manipulate control states. The chosen values make sure,
-				// detail page is busy indication immediately so there is no break in
-				// between the busy indication for loading the view's meta data
-				var iOriginalBusyDelay,
-					oViewModel = new JSONModel({
-						busy : true,
-						delay : 0
-					});
+        this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
 
-				this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
+        iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
+        this.setModel(oViewModel, "objectView");
+        this.getOwnerComponent().getModel().metadataLoaded().then(function () {
+            oViewModel.setProperty("/delay", iOriginalBusyDelay);
+          }
+        );
+      },
 
-				// Store original busy indicator delay, so it can be restored later on
-				iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
-				this.setModel(oViewModel, "objectView");
-				this.getOwnerComponent().getModel().metadataLoaded().then(function () {
-						// Restore original busy indicator delay for the object view
-						oViewModel.setProperty("/delay", iOriginalBusyDelay);
-					}
-				);
-			},
+      onNavBack : function() {
+        var sPreviousHash = History.getInstance().getPreviousHash();
 
-			/* =========================================================== */
-			/* event handlers                                              */
-			/* =========================================================== */
+        if (sPreviousHash !== undefined) {
+          history.go(-1);
+        } else {
+          this.getRouter().navTo("worklist", {}, true);
+        }
+      },
 
+      _onObjectMatched : function (oEvent) {
+        var sObjectId =  oEvent.getParameter("arguments").objectId;
+        this.getModel().metadataLoaded().then( function() {
+          var sObjectPath = this.getModel().createKey("MaterialID", {
+            ID :  sObjectId
+          });
+          this._bindView("/" + sObjectPath);
+        }.bind(this));
+      },
 
-			/**
-			 * Event handler  for navigating back.
-			 * It there is a history entry we go one step back in the browser history
-			 * If not, it will replace the current entry of the browser history with the worklist route.
-			 * @public
-			 */
-			onNavBack : function() {
-				var sPreviousHash = History.getInstance().getPreviousHash();
+      _bindView : function (sObjectPath) {
+        var oViewModel = this.getModel("objectView"),
+          oDataModel = this.getModel();
 
-				if (sPreviousHash !== undefined) {
-					history.go(-1);
-				} else {
-					this.getRouter().navTo("worklist", {}, true);
-				}
-			},
+        this.getView().bindElement({
+          path: sObjectPath,
+          events: {
+            change: this._onBindingChange.bind(this),
+            dataRequested: function () {
+              oDataModel.metadataLoaded().then(function () {
+                oViewModel.setProperty("/busy", true);
+              });
+            },
+            dataReceived: function () {
+              oViewModel.setProperty("/busy", false);
+            }
+          }
+        });
+      },
 
-			/* =========================================================== */
-			/* internal methods                                            */
-			/* =========================================================== */
+      _onBindingChange : function () {
+        var oView = this.getView(),
+          oViewModel = this.getModel("objectView"),
+          oElementBinding = oView.getElementBinding();
 
-			/**
-			 * Binds the view to the object path.
-			 * @function
-			 * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
-			 * @private
-			 */
-			_onObjectMatched : function (oEvent) {
-				var sObjectId =  oEvent.getParameter("arguments").objectId;
-				this.getModel().metadataLoaded().then( function() {
-					var sObjectPath = this.getModel().createKey("MaterialID", {
-						ID :  sObjectId
-					});
-					this._bindView("/" + sObjectPath);
-				}.bind(this));
-			},
+        if (!oElementBinding.getBoundContext()) {
+          this.getRouter().getTargets().display("objectNotFound");
+          return;
+        }
 
-			/**
-			 * Binds the view to the object path.
-			 * @function
-			 * @param {string} sObjectPath path to the object to be bound
-			 * @private
-			 */
-			_bindView : function (sObjectPath) {
-				var oViewModel = this.getModel("objectView"),
-					oDataModel = this.getModel();
+        oViewModel.setProperty("/busy", false);
+      }
 
-				this.getView().bindElement({
-					path: sObjectPath,
-					events: {
-						change: this._onBindingChange.bind(this),
-						dataRequested: function () {
-							oDataModel.metadataLoaded().then(function () {
-								// Busy indicator on view should only be set if metadata is loaded,
-								// otherwise there may be two busy indications next to each other on the
-								// screen. This happens because route matched handler already calls '_bindView'
-								// while metadata is loaded.
-								oViewModel.setProperty("/busy", true);
-							});
-						},
-						dataReceived: function () {
-							oViewModel.setProperty("/busy", false);
-						}
-					}
-				});
-			},
+    });
 
-			_onBindingChange : function () {
-				var oView = this.getView(),
-					oViewModel = this.getModel("objectView"),
-					oElementBinding = oView.getElementBinding();
-
-				// No data for the binding
-				if (!oElementBinding.getBoundContext()) {
-					this.getRouter().getTargets().display("objectNotFound");
-					return;
-				}
-
-				var oResourceBundle = this.getResourceBundle(),
-					oObject = oView.getBindingContext().getObject(),
-					sObjectId = oObject.ID,
-					sObjectName = oObject.ID;
-
-				oViewModel.setProperty("/busy", false);
-
-				oViewModel.setProperty("/shareSendEmailSubject",
-				oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
-				oViewModel.setProperty("/shareSendEmailMessage",
-				oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
-			}
-
-		});
-
-	}
+  }
 );
